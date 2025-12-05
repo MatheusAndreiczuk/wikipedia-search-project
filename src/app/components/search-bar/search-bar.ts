f4b8591b771e09f9bd9891130df7acb886377c4b
MatheusAndreiczuk <matheusandreizuk@gmail.com>
2025-12-05 01:38:28 -0300

    feat: função de limpar input de pesquisa; fix: agora o hover só é aplicado no link da pesquisa, não mais na linha inteira

--- src/app/components/search-bar/search-bar.ts
+++ src/app/components/search-bar/search-bar.ts
@@ -1,6 +1,6 @@
 import { SearchService } from './../../services/search-service';
-import { Component, inject, output, signal } from '@angular/core';
-import { LucideAngularModule, Search, Star } from "lucide-angular";
+import { Component, ElementRef, inject, output, signal, ViewChild } from '@angular/core';
+import { LucideAngularModule, Search, Star, X } from "lucide-angular";
 
 @Component({
   selector: 'app-search-bar',
@@ -9,12 +9,30 @@ import { LucideAngularModule, Search, Star } from "lucide-angular";
   styleUrl: './search-bar.css',
 })
 export class SearchBar {
+  @ViewChild('searchInputRef') searchInputRef!: ElementRef<HTMLInputElement>;
   readonly searchService = inject(SearchService);
   readonly searchIcon = Search;
   readonly starIcon = Star;
+  readonly xIcon = X;
 
   search = output<string>();
   searchInput = signal('');
+  isInputFocused = signal(false);
+
+  onFocus(){
+    this.isInputFocused.set(true);
+  }
+
+  onBlur(){
+    this.isInputFocused.set(false);
+  }
+
+  clearInput(){
+    this.searchInput.set('');
+    setTimeout(() => {
+      this.searchInputRef.nativeElement.focus();
+    });
+  }
 
   onSearch(event: Event, term: string) {
     event.preventDefault();
